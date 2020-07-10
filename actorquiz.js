6ad48605a56d4bbb821965fd70d27b740c206bd5
Superjay813 <46241335+Superjay813@users.noreply.github.com>
2020-07-10 14:38:09 +0100

    Add files via upload

---
+++ actorquiz.js
@@ -0,0 +1,128 @@
+var ul=document.getElementById('ul');
+var btn=document.getElementById('button');
+var scoreCard=document.getElementById('scoreCard');
+var quizBox=document.getElementById('questionBox');
+var op1=document.getElementById('op1');
+var op2=document.getElementById('op2');
+var op3=document.getElementById('op3');
+var op4=document.getElementById('op4');
+
+      var app={
+                questions:[
+                          {q:'Who plays Anne Cuthbert?', options:['Amybeth McDonald','Germany annexed Austria','ss','Amybeth McNulty'],answer:3},
+
+
+                          {q:'Who plays Gilbert Blythe?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Diana Barry?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Matthew Cuthbert?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Marilla Cuthbert?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Rachel Lynde?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Jerry Baynard?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Sebastian "Bash"?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Cole Mackenzie?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Miss Stacy?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Billy Andrews?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Prissy Andrews?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Mr Phillips?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Josie Pye?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Tillie Boulter?',options:['1933','1935','1939','1941'],answer:3},
+
+
+                          {q:'Who plays Mary Lacroix?',options:['1933','1935','1939','1941'],answer:3},
+
+                          ],
+
+                index:0,
+                load:function(){
+                	   if(this.index<=this.questions.length-1){
+                        quizBox.innerHTML=this.index+1+". "+this.questions[this.index].q;      
+                        op1.innerHTML=this.questions[this.index].options[0];
+                        op2.innerHTML=this.questions[this.index].options[1];
+                        op3.innerHTML=this.questions[this.index].options[2];
+                        op4.innerHTML=this.questions[this.index].options[3];
+                           this.scoreCard();
+                        }
+
+                        else{
+                        quizBox.innerHTML="Welldone You Have Completed this Quiz! Click Below for more or to get back to home."      
+                        op1.style.display="none";
+                        op2.style.display="none";
+                        op3.style.display="none";
+                        op4.style.display="none";
+                        btn.style.display="none";
+                        }
+                },
+
+                 next:function(){
+                    this.index++;
+                    this.load();
+                 },
+
+                check:function(ele){
+                         var id=ele.id.split('');
+                         if(id[id.length-1]==this.questions[this.index].answer){
+                         	this.score++;
+                         	ele.className="correct";
+                         	ele.innerHTML="Correct";
+                         	this.scoreCard();
+                         }
+
+                         else{
+                         	ele.className="wrong";
+                         	ele.innerHTML="Wrong";
+                         }
+                },
+                notClickAble:function(){
+                       for(let i=0;i<ul.children.length;i++){
+                       	    ul.children[i].style.pointerEvents="none";
+                       }
+                },
+
+                clickAble:function(){
+                       for(let i=0;i<ul.children.length;i++){
+                       	    ul.children[i].style.pointerEvents="auto";
+                       	    ul.children[i].className=''
+                       }
+                },
+
+                score:0,
+                scoreCard:function(){
+                	scoreCard.innerHTML=this.score+"/"+this.questions.length;
+                }
+           }
+           window.onload=app.load();
+           function button(ele){
+           	     app.check(ele);
+           	     app.notClickAble();
+           }
+
+         function  next(){
+              app.next();
+              app.clickAble();
+         } 
